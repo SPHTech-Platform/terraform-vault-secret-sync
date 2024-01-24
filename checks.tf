fedce31f65590d9e3b6019936bc9150960384b0c
Hazmei Abdul Rahman <hazmei@sph.com.sg>
2024-01-24 15:31:59 +0800

    Fix iam user name

--- checks.tf
+++ checks.tf
@@ -4,6 +4,6 @@ check "check_iam_key_age_vault_secretsync" {
       timecmp(coalesce(aws_iam_access_key.vault_secretsync.create_date, local.age_in_days), local.age_in_days) > 0
     )
     error_message = format("The IAM key for metrics user %s is older than 40 days. Please rotate the key.",
-    aws_iam_user.vault_secretsync.name)
+    module.iam_user_secretsync.iam_user_name)
   }
 }
